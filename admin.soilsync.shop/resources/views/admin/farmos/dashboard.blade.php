8146b1cbff87d704b4ffbd1a0bcf5eac1f3d88a3
SoilSync Admin <admin@soilsync.shop>
2026-01-08 00:19:11 +0000

    Show full farmOS UI with sidebar in iframes
    Removed ?iframe_embed=1 parameter from iframe URLs.
    
    Now that iframe embedding works properly with CSP headers fixed,
    users get the full farmOS experience including:
    - farmOS sidebar navigation
    - User menu
    - Full farmOS interface
    
    This provides better UX - users can navigate farmOS naturally
    within the admin portal iframe.

--- admin.soilsync.shop/resources/views/admin/farmos/dashboard.blade.php
+++ admin.soilsync.shop/resources/views/admin/farmos/dashboard.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container-fluid p-0" style="height: calc(100vh - 60px);">
     <iframe 
-        src="{{ config('farmos.url') }}/?iframe_embed=1" 
+        src="{{ config('farmos.url') }}/" 
         style="width: 100%; height: 100%; border: none;"
         title="farmOS Dashboard"
     ></iframe>
--- admin.soilsync.shop/resources/views/admin/farmos/harvests.blade.php
+++ admin.soilsync.shop/resources/views/admin/farmos/harvests.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container-fluid p-0" style="height: calc(100vh - 60px);">
     <iframe 
-        src="{{ config('farmos.url') }}/logs/harvest?iframe_embed=1" 
+        src="{{ config('farmos.url') }}/logs/harvest" 
         style="width: 100%; height: 100%; border: none;"
         title="farmOS Harvest Logs"
     ></iframe>
